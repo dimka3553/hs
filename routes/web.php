4bb8d0a7c9d10a6ca21140046551e1a098fd2cf4
dimka3553 <37512400+dimka3553@users.noreply.github.com>
2022-11-09 09:10:42 +0100

    Added a route to the HelloController

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HelloController::class, 'index']);
